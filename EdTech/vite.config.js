6519f44fdf298017dd671b5582fdefe72ed60653
JitendraVarma11 <vjitendra1192002@gmail.com>
2024-04-14 11:45:54 +0530

    update vite config

--- EdTech/vite.config.js
+++ EdTech/vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:3000},
+  define: {
+    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
+    'process.env.VITE_RAZORPAY_KEY': JSON.stringify(process.env.VITE_RAZORPAY_KEY),
+    
+  },
 })
