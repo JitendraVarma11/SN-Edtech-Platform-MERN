28783f9929472aa0967046f3030c29fe3ff1c355
Jitendra Varma <143241523+JitendraVarma11@users.noreply.github.com>
2024-04-14 12:03:46 +0530

    Update vite.config.js

--- EdTech/vite.config.js
+++ EdTech/vite.config.js
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{port:3000},
-  define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(import.meta.env.VITE_BASE_URL),
-    'process.env.VITE_RAZORPAY_KEY': JSON.stringify(import.meta.env.VITE_ROZARPAY_KEY), 
-  },
 })
