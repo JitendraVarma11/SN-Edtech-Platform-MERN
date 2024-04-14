f686438de26b9d55ea5647d207f8383b6ab2c283
JitendraVarma11 <vjitendra1192002@gmail.com>
2024-04-14 11:52:13 +0530

    update vite congfig

--- EdTech/vite.config.js
+++ EdTech/vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server:{port:3000},
   define: {
-    'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL),
-    'process.env.VITE_RAZORPAY_KEY': JSON.stringify(process.env.VITE_RAZORPAY_KEY),
-    
+    'process.env.VITE_BASE_URL': JSON.stringify(import.meta.env.VITE_BASE_URL),
+    'process.env.VITE_RAZORPAY_KEY': JSON.stringify(import.meta.env.VITE_ROZARPAY_KEY), 
   },
 })
