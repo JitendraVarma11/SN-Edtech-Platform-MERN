27bfb0884b6cc46f25b1ec69155ba6dbd97ba63c
Jitendra Varma <143241523+JitendraVarma11@users.noreply.github.com>
2024-04-29 12:07:55 +0530

    Update apis.js

--- EdTech/src/services/apis.js
+++ EdTech/src/services/apis.js
@@ -1,5 +1,4 @@
-const BASE_URL=import.meta.env.VITE_BASE_URL
-
+const BASE_URL="https://sn-edtech-platform.vercel.app/api/v1"
 // AUTH ENDPOINTS
 export const endpoints = {
   SENDOTP_API: BASE_URL + "/auth/sendotp",
